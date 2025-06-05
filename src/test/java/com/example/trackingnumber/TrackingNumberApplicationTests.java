14ce8c8cef38a27a7d9e9d6ac284c1e4cab83dc6
Fitra Aditama <fitraaditama7@gmail.com>
2025-06-05 17:52:42 +0700

    fix tests

--- src/test/java/com/example/trackingnumber/TrackingNumberApplicationTests.java
+++ src/test/java/com/example/trackingnumber/TrackingNumberApplicationTests.java
@@ -2,8 +2,16 @@ package com.example.trackingnumber;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.liquibase.enabled=false"
+})
 class TrackingNumberApplicationTests {
 
     @Test
